35d6c8b9c605813658c26ad8b2f6a0f2b23b7c53
larisakozina100 <larisa.kozina100@gmail.com>
2026-05-01 00:17:44 +0300

    extract locators

--- tests/test_shop.py
+++ tests/test_shop.py
@@ -1,12 +1,13 @@
 from playwright.sync_api import Page, expect
 from utils.auth import login
+from utils.locators import CART_BADGE, ADD_BACKPACK_BUTTON
 
 def test_add_item_to_cart(page: Page) -> None:
     login(page)
 
-    expect(page).to_have_url('https://www.saucedemo.com/inventory.html')
+    expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
 
-    page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click()
-    
-    expect(page.locator('.shopping_cart_badge')).to_have_text('1')
+    page.locator(ADD_BACKPACK_BUTTON).click()
+
+    expect(page.locator(CART_BADGE)).to_have_text("1")
 
---
+++ utils/locators.py
@@ -0,0 +1,3 @@
+CART_BADGE = '[data-test="shopping-cart-badge"]'
+ADD_BACKPACK_BUTTON = '[data-test="add-to-cart-sauce-labs-backpack"]'
+ERROR_MESSAGE = '[data-test="error"]'
