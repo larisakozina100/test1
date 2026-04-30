683d5838fe90964f265d60a522329db866e08715
larisakozina100 <larisa.kozina100@gmail.com>
2026-04-30 16:09:24 +0300

    fix python path fot utils import

--- tests/test_shop.py
+++ tests/test_shop.py
@@ -1,14 +1,10 @@
 from playwright.sync_api import Page, expect
+from utils.auth import login
 
 def test_add_item_to_cart(page: Page) -> None:
-    page.goto('https://www.saucedemo.com/')
-
-    page.get_by_placeholder('Username').fill('standard_user')
-    page.get_by_placeholder('Password').fill('secret_sauce')
-    page.get_by_role('button', name='Login').click()
+    login(page)
 
     expect(page).to_have_url('https://www.saucedemo.com/inventory.html')
-    expect(page.get_by_text('Products')).to_be_visible()
 
     page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click()
     
