424bf3a8f421e158e829531ef126a710b37c678d
larisakozina100 <larisa.kozina100@gmail.com>
2026-05-02 20:51:55 +0300

    extract route constants

--- tests/test_shop.py
+++ tests/test_shop.py
@@ -1,12 +1,12 @@
 from playwright.sync_api import expect
 from utils.locators import CART_BADGE, ADD_BACKPACK_BUTTON
+from utils.routes import INVENTORY_PATH
 
-def test_add_item_to_cart(logged_in_page):
+def test_add_item_to_cart(logged_in_page, base_url: str) -> None:
     page = logged_in_page
 
-    expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
+    expect(page).to_have_url(base_url + INVENTORY_PATH)
 
     page.locator(ADD_BACKPACK_BUTTON).click()
 
     expect(page.locator(CART_BADGE)).to_have_text("1")
-
