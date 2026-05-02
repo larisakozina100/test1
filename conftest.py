566a228a436ac7a1c5003a53318c5683e55f2a6c
larisakozina100 <larisa.kozina100@gmail.com>
2026-05-02 20:36:20 +0300

    add base_url config

--- conftest.py
+++ conftest.py
@@ -1,9 +1,8 @@
+import pytest
 from playwright.sync_api import Page
 from utils.auth import login
 
-import pytest
-
 @pytest.fixture
-def logged_in_page(page: Page) -> Page:
-    login(page)
+def logged_in_page(page: Page, base_url: str) -> Page:
+    login(page, base_url)
     return page
--- utils/auth.py
+++ utils/auth.py
@@ -1,7 +1,7 @@
 from playwright.sync_api import Page
 
-def login(page: Page) -> None:
-    page.goto("https://www.saucedemo.com/")
+def login(page: Page, base_url: str) -> None:
+    page.goto(base_url)
     page.get_by_placeholder('Username').fill('standard_user')
     page.get_by_placeholder('Password').fill('secret_sauce')
     page.get_by_role('button', name='Login').click()
